910dc4b34d09e2bf15ff4e360eadd125b9b9ea6a
FenixPyro <leoezcar@gmail.com>
2024-07-04 05:50:23 -0300

    Fixed issues and updated project to ensure functionality and pass tests

--- medical_exam_data_visualizer/medical_data_visualizer.py
+++ medical_exam_data_visualizer/medical_data_visualizer.py
@@ -3,61 +3,40 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = pd.read_csv('medical_exam_data_visualizer/medical_examination.csv')
+df = pd.read_csv('medical_exam_data_visualizer\medical_examination.csv')
 
-# 2
 BMI = (df['weight']) / (df['height'] / 100)**2
 overweight_check = BMI > 25
 df['overweight'] = overweight_check.astype(int)
 
-# 3
 cholesterol_check = df['cholesterol'] != 1
 gluc_check = df['gluc'] != 1
 df['cholesterol'] = cholesterol_check.astype(int)
 df['gluc'] = gluc_check.astype(int)
 
 def draw_cat_plot():
-    df_cat = pd.melt(df, id_vars = ['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'],
-                 var_name='variable', value_name='value')
-    fig = sns.catplot(x='variable', hue='value', col='cardio', data=df_cat, kind='count')
+    df_cat = pd.melt(df, id_vars = ['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
+    df_cat['total']= 1
+    df_cat = df_cat.groupby(['cardio','variable','value'], as_index = False).count()
+    fig = sns.catplot(x='variable', y = 'total', hue='value', col='cardio', data=df_cat, kind='bar')
+    fig = fig.figure
     fig.savefig('catplot.png')
     return fig
 
-# filter_1 = df['height'] >= df['height'].quantile(0.025)
-# filter_2 = df['height'] <= df['height'].quantile(0.975)
-# filter_3 = df['weight'] >= df['weight'].quantile(0.025)
-# filter_3 = df['weight'] <= df['weight'].quantile(0.975)
-# filter = filter_1 & filter_2 & filter_3 & filter_4
-# df_cleaned_1 = df[filter]
-
 # 10
 def draw_heat_map():
-    # 11
+
     filter_1 = df['height'] >= df['height'].quantile(0.025)
     filter_2 = df['height'] <= df['height'].quantile(0.975)
     filter_3 = df['weight'] >= df['weight'].quantile(0.025)
     filter_4 = df['weight'] <= df['weight'].quantile(0.975)
-    filter = filter_1 & filter_2 & filter_3 & filter_4
-    df_heat = df[filter]
+    df_filter = filter_1 & filter_2 & filter_3 & filter_4
+    df_heat = df[df_filter]
 
-    # 12
     corr = df_heat.corr()
-
-    # 13
-    mask = np.triu(np.ones_like(corr, dtype=bool))
-
-
-    # 14
-    fig, ax = plt.subplots(figsize=(8, 6))
-    sns.heatmap(corr, mask = mask, ax=ax, fmt='.1f', annot=True, cmap = 'coolwarm')
-
-    # 15
-
-
-
-    # 16
+    mask = np.triu(corr)
+    fig, ax = plt.subplots(figsize=(8, 8))
+    sns.heatmap(corr, linewidths =1, square = True, ax=ax, mask = mask, fmt='.1f', annot=True)
+    fig = fig.figure
     fig.savefig('heatmap.png')
     return fig
-
-print(draw_heat_map())
--- medical_exam_data_visualizer/test_module.py
+++ medical_exam_data_visualizer/test_module.py
@@ -36,7 +36,7 @@ class HeatMapTestCase(unittest.TestCase):
     def test_heat_map_labels(self):
         actual = []
         for label in self.ax.get_xticklabels():
-          actual.append(label.get_text())
+            actual.append(label.get_text())
         expected = ['id', 'age', 'sex', 'height', 'weight', 'ap_hi', 'ap_lo', 'cholesterol', 'gluc', 'smoke', 'alco', 'active', 'cardio', 'overweight']
         self.assertEqual(actual, expected, "Expected heat map labels to be 'id', 'age', 'sex', 'height', 'weight', 'ap_hi', 'ap_lo', 'cholesterol', 'gluc', 'smoke', 'alco', 'active', 'cardio', 'overweight'.")
     
@@ -45,6 +45,12 @@ class HeatMapTestCase(unittest.TestCase):
         print(actual)
         expected = ['0.0', '0.0', '-0.0', '0.0', '-0.1', '0.5', '0.0', '0.1', '0.1', '0.3', '0.0', '0.0', '0.0', '0.0', '0.0', '0.0', '0.2', '0.1', '0.0', '0.2', '0.1', '0.0', '0.1', '-0.0', '-0.1', '0.1', '0.0', '0.2', '0.0', '0.1', '-0.0', '-0.0', '0.1', '0.0', '0.1', '0.4', '-0.0', '-0.0', '0.3', '0.2', '0.1', '-0.0', '0.0', '0.0', '-0.0', '-0.0', '-0.0', '0.2', '0.1', '0.1', '0.0', '0.0', '0.0', '0.0', '0.3', '0.0', '-0.0', '0.0', '-0.0', '-0.0', '-0.0', '0.0', '0.0', '-0.0', '0.0', '0.0', '0.0', '0.2', '0.0', '-0.0', '0.2', '0.1', '0.3', '0.2', '0.1', '-0.0', '-0.0', '-0.0', '-0.0', '0.1', '-0.1', '-0.1', '0.7', '0.0', '0.2', '0.1', '0.1', '-0.0', '0.0', '-0.0', '0.1']
         self.assertEqual(actual, expected, "Expected different values in heat map.")
+
+class HeatMapTestCase(unittest.TestCase):
+    def setUp(self):
+        self.fig = medical_data_visualizer.draw_heat_map()
+        self.ax = self.fig.axes[0]
+        self.maxDiff = None
 
 if __name__ == "__main__":
     unittest.main()
